dbf919f2084720b87113f58606b20e73e897bf7f
Yueyang Chen <yychen16@hotmail.com>
2019-03-20 10:33:28 -0700

    Contrast Adjustment Py

---
+++ nanoBALLS/contrast_adj.py
@@ -0,0 +1,15 @@
+def adjustment(ImageName, Alpha=1.7, Beta=0):
+    # Alphafactor for contrast control
+    # Beta for brightness control
+
+    import numpy as np
+    import cv2
+
+    img = cv2.imread(ImageName)
+    img = np.array(img, dtype=np.uint8)
+    img2 = cv2.convertScaleAbs(img, alpha=Alpha, beta=Beta)
+    kernel = np.ones((5, 5), np.uint8)
+    opening2 = cv2.morphologyEx(img2, cv2.MORPH_OPEN, kernel)
+    canny2 = cv2.Canny(opening2, 100, 150, 3, L2gradient=True)
+
+    return canny2
