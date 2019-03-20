f931a04d25c78e0d21836aeea9b645ff29930eb8
Omkar Bhambure <omkarb@uw.edu>
2019-03-20 14:08:40 -0700

    Changed the name of the functions

--- nanoBALLS/tests/test_Shape_fitting.py
+++ nanoBALLS/tests/test_Shape_fitting.py
@@ -12,12 +12,12 @@ kernel = np.ones((5,5), np.uint8)
 opening = cv2.morphologyEx(img2, cv2.MORPH_OPEN, kernel)
 canny_image = cv2.Canny(opening,100,150,3,L2gradient=True)
 mean_contour_Area = 1200
-x=(Shape_fitting.Get_Ellipse(canny_image,mean_contour_Area))
+x=(Shape_fitting.get_ellipse(canny_image,mean_contour_Area))
 major_axis=x[0]
 minor_axis=x[1]
-def test_Get_Ellipse():
+def test_get_ellipse():
     """A funtion that carries the unittests for Get_Ellipse function """
-    assert type(Shape_fitting.Get_Ellipse(canny_image,mean_contour_Area))==tuple,"Expecting a tuple"
+    assert type(Shape_fitting.get_ellipse(canny_image,mean_contour_Area))==tuple,"Expecting a tuple"
     major_axis=x[0]
     minor_axis=x[1]
     assert len(major_axis)==len(minor_axis),"length of the both the lists is not same"
@@ -25,8 +25,8 @@ def test_Get_Ellipse():
         assert major_axis[i]>minor_axis[i],"major axis must be greater tha minor axis"
     return
 
-def test_Predict_Shape():
+def test_predict_shape():
     """A funtion that carries the unittests for Predict_Shape function """
-    assert type(Shape_fitting.Predict_Shape(major_axis,minor_axis))==tuple,"Expecting a tuple"
-    assert Shape_fitting.Predict_Shape(major_axis,minor_axis)[0]+Shape_fitting.Predict_Shape(major_axis,minor_axis)[1]==len(major_axis),"All the elements must be classified as circle or ellipse "
+    assert type(Shape_fitting.predict_shape(major_axis,minor_axis))==tuple,"Expecting a tuple"
+    assert Shape_fitting.predict_shape(major_axis,minor_axis)[0]+Shape_fitting.predict_shape(major_axis,minor_axis)[1]==len(major_axis),"All the elements must be classified as circle or ellipse "
     return
