e1e3ccbdfd029fa4e280ead5cc214ffc81a85cba
Rachel Gebhart <rgebhart@uw.edu>
2019-03-20 11:27:23 -0700

    Update segmentparser.py
    Updating so that this code should run and starting to add in citations.

--- nanoBALLS/segmentparser.py
+++ nanoBALLS/segmentparser.py
@@ -1,7 +1,12 @@
 import cv2
 import numpy as np
+import pandas as pd
+from skimage.measure import regionprops
 
 def segmentparser(segmented_image, binary):
+    """Docstring.
+    
+    Code courtesy Chad Curtis, pulled from L9 Image Proccessing Lecture"""
     
     props = regionprops(segmented_image, intensity_image=binary)
     x = y = area = perimeter = intensity = np.zeros(len(props))
@@ -9,9 +14,9 @@ def segmentparser(segmented_image, binary):
     index_i = 0
 
     for index_j in props:
-        x[index_i] = prop.centroid[0]
-        y[index_i] = prop.centroid[1]
-        area[index_i] = prop.area
+        x[index_i] = index_j.centroid[0]
+        y[index_i] = index_j.centroid[1]
+        area[index_i] = index_j.area
         
         index_i = index_i + 1
     
