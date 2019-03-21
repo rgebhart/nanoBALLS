28f00ae6689acc3fa3b82879640e2774cda8d9b7
isaack8 <46461561+isaack8@users.noreply.github.com>
2019-03-21 11:26:45 -0700

    Made dynamic maybe

--- nanoballs/__init__.py
+++ nanoballs/__init__.py
@@ -1,1 +1,4 @@
-__all__ = ["circle", "contras_adj", "convert", "imageseg", "segmentparser", "shape_fitting"]
+from os.path import dirname, basename, isfile
+import glob
+modules = glob.glob(dirname(__file__)+"/*.py")
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
