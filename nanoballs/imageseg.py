971792df199b76718640ecd669c61b5442f0d093
Isaac Kretzmer <isaack8@uw.edu>
2019-03-21 14:46:56 -0700

    changes?

--- nanoballs/imageseg.py
+++ nanoballs/imageseg.py
@@ -7,7 +7,27 @@ from skimage.morphology import watershed
 from scipy import ndimage
 import argparse
 from skimage.measure import label
-from segmentparser import segmentparser
+
+def segmentparser(segmented_image, binary):
+    """Docstring.
+    
+    Code courtesy Chad Curtis, pulled from L9 Image Proccessing Lecture"""
+    
+    props = regionprops(segmented_image, intensity_image=binary)
+    x = y = area = perimeter = intensity = np.zeros(len(props))
+
+    index_i = 0
+
+    for index_j in props:
+        x[index_i] = index_j.centroid[0]
+        y[index_i] = index_j.centroid[1]
+        area[index_i] = index_j.area
+        
+        index_i = index_i + 1
+    
+    segment_properties = pd.DataFrame({'X': x, 'Y': y, 'Area': area})
+
+    return segment_properties
 
 def imageseg(Cont_Image):
     """imageseg('Image Name')
