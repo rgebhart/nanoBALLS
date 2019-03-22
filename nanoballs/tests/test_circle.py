3f7ac23bafee15d476626567c61a771f988c61e5
Omkar Bhambure <omkarb@uw.edu>
2019-03-22 00:10:47 -0700

    Still getting an error

--- nanoballs/tests/test_circle.py
+++ nanoballs/tests/test_circle.py
@@ -1,11 +1,11 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-import circle
+from nanoballs import circle
 def test_get_circles():
 	""" Test function for get_circle function"""
 	img = cv2.imread("sem_images/Opal_Tecopa_near_gem.jpg",0) 
 	img = cv2.medianBlur(img,5)
-	circles = circle.get_circles(img,3,20,150,50,0,30)
+	circles = circle.GetCircles(img,3,20,150,50,0,30)
 	assert type(circles) == tuple
 	return
