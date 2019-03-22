9918070a409100cc643f438f3ff64eba72b88649
Rachel Gebhart <rgebhart@uw.edu>
2019-03-22 00:13:56 -0700

    Update test_Shape_fitting.py
    Changed import - hopefully it fixes things now.

--- nanoballs/tests/test_Shape_fitting.py
+++ nanoballs/tests/test_Shape_fitting.py
@@ -1,4 +1,4 @@
-from nanoballs import Shape_fitting
+from nanoballs import shape_fitting
 import cv2
 import numpy as np
 img = cv2.imread("C:/Users/Omkar/Desktop/nanoBALLS/sem_images/Opal_Tecopa_near_gem")
@@ -11,13 +11,13 @@ kernel = np.ones((5,5), np.uint8)
 opening = cv2.morphologyEx(img2, cv2.MORPH_OPEN, kernel)
 canny_image = cv2.Canny(opening,100,150,3,L2gradient=True)
 mean_contour_Area = 1200
-x=(Shape_fitting.get_ellipse(canny_image,mean_contour_Area))
+x=(shape_fitting.get_ellipse(canny_image,mean_contour_Area))
 major_axis=x[0]
 minor_axis=x[1]
 
 def test_get_ellipse():
     """A funtion that carries the unittests for Get_Ellipse function """
-    assert type(Shape_fitting.get_ellipse(canny_image,mean_contour_Area))==tuple,"Expecting a tuple"
+    assert type(shape_fitting.get_ellipse(canny_image,mean_contour_Area))==tuple,"Expecting a tuple"
     major_axis=x[0]
     minor_axis=x[1]
     assert len(major_axis)==len(minor_axis),"length of the both the lists is not same"
@@ -27,6 +27,6 @@ def test_get_ellipse():
 
 def test_predict_shape():
     """A funtion that carries the unittests for Predict_Shape function """
-    assert type(Shape_fitting.predict_shape(major_axis,minor_axis))==tuple,"Expecting a tuple"
-    assert Shape_fitting.predict_shape(major_axis,minor_axis)[0]+Shape_fitting.predict_shape(major_axis,minor_axis)[1]==len(major_axis),"All the elements must be classified as circle or ellipse "
+    assert type(shape_fitting.predict_shape(major_axis,minor_axis))==tuple,"Expecting a tuple"
+    assert shape_fitting.predict_shape(major_axis,minor_axis)[0]+shape_fitting.predict_shape(major_axis,minor_axis)[1]==len(major_axis),"All the elements must be classified as circle or ellipse "
     return
